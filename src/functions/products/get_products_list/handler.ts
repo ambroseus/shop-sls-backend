510fa99b7c756cf9aa3050551a828bb07457002f
Eugene Samonenko <ambroseus@gmail.com>
2022-07-17 18:05:34 +0300

    added get_products_list handler

--- src/functions/products/get_products_list/handler.ts
+++ src/functions/products/get_products_list/handler.ts
@@ -1,12 +1,15 @@
-import { middyfy } from '../../../utils/lambda'
+import { middyfy, formatJSONResponse } from '../../../utils/lambda'
 import { loggers, errorMessage } from '../../../utils/logger'
+
+import { getProductsList } from '../../../services/products/getProductsList'
 
 const { ERROR } = loggers('get_products_list')
 
 export const get_products_list = async () => {
   try {
-    // await checkServiceState()
-    return { statusCode: 200, body: 'OK' }
+    const products = await getProductsList()
+    return formatJSONResponse(products)
+    //
   } catch (e) {
     ERROR(errorMessage(e))
     return { statusCode: 500 }
---
+++ src/models/Product.ts
@@ -0,0 +1,6 @@
+export type Product = {
+  productId: string
+  name: string
+  description: string
+  price: number
+}

31a172e4d1d915ca517a4a5d4238f565ca9f820c
Eugene Samonenko <ambroseus@gmail.com>
2022-07-17 21:55:51 +0300

    update dependencies

--- src/functions/products/get_product_by_id/index.ts
+++ src/functions/products/get_product_by_id/index.ts
@@ -1,5 +1,5 @@
 export default {
-  handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.handler`,
+  handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
   events: [
     {
       http: {

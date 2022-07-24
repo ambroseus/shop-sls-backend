8d1183121012e96dbee378b72e0d4a33b15fb67e
Eugene Samonenko <ambroseus@gmail.com>
2022-07-24 23:08:13 +0300

    fixed cors

--- src/services/products/getProductsList.ts
+++ src/services/products/getProductsList.ts
@@ -1,7 +1,7 @@
 import { Product } from '../../models/Product'
 import { dbQuery } from '../../libs/db'
 
-const query = 'select id,title,description,price,count from products join stocks on id=product_id'
+const query = 'select id,title,description,price,count from products join stocks on id=product_id order by id'
 
 export const getProductsList = async (limit?: number) => {
   const withLimit = limit ? ` limit ${limit}` : ''

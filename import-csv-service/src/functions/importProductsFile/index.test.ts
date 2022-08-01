b5b071a41f197c953fdb1a70e78cc713f450ef4b
Eugene Samonenko <ambroseus@gmail.com>
2022-08-01 13:01:34 +0300

    added test

---
+++ import-csv-service/src/functions/importProductsFile/index.test.ts
@@ -0,0 +1,33 @@
+jest.mock('@aws-sdk/client-s3')
+jest.mock('@aws-sdk/s3-request-presigner')
+
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { importProductsFile } from './handler'
+;(global.console as any) = { error: jest.fn(), log: jest.fn() }
+
+describe('importProductsFile handler', () => {
+  it('should return error if no name in query params', async () => {
+    //jest.spyOn(svcProduct, 'getProductById').mockResolvedValue(productsList[0])
+
+    const result: any = await importProductsFile({ queryStringParameters: {} })
+
+    expect(result.statusCode).toEqual(400)
+  })
+
+  it('should return signed url', async () => {
+    const getSignedUrlMock: jest.Mock = getSignedUrl as any
+
+    const testUrl = 'testUrl'
+    getSignedUrlMock.mockResolvedValue(testUrl)
+
+    const result: any = await importProductsFile({ queryStringParameters: { name: 'test.csv' } })
+
+    expect(result).toEqual({
+      statusCode: 200,
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: `"${testUrl}"`,
+    })
+  })
+})
